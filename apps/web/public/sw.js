f217d9b1309a11b9dd2ca8af76ccafac6122fb3a
vanechka1989 <vanechka1989@users.noreply.github.com>
2026-07-26 18:54:10 +0700

    chore: prepare release 5.63

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "club-pwa-v234";
+const cacheName = "club-pwa-v235";
 const appShell = ["/manifest.webmanifest", "/icons/icon.svg", "/offline.html"];
 
 self.addEventListener("install", (event) => {
